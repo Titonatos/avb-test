867c592161297b94cbb51e7cfdd2645c7f352412
Titonatos <titanium4ik777@gmail.com>
2025-08-02 20:43:52 +0300

    init project

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Header () {
+	return (
+		<header>
+			<Link href=""></Link>
+			<nav>
+				<Link href=""></Link>
+			</nav>
+		</header>
+	);
+};
